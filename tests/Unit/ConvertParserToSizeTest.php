dd57d9eb130f370e994c433383217371bf006e07
Lucas <lucasjs.developer@gmail.com>
2024-11-02 09:02:38 -0300

    updated tests

--- tests/Unit/ConvertParserToSizeTest.php
+++ tests/Unit/ConvertParserToSizeTest.php
@@ -2,14 +2,14 @@
 
 use Lucasjs7\ConvertDataSize\ConvertDataSize;
 
-it('ConvertTo#KiB->MB', function () {
+it('Parser#KiB->MB', function () {
     expect('78384470540.63')->toBe(ConvertDataSize::parser('76547334512342 KiB', 'MB'));
 });
 
-it('ConvertTo#EB->GB', function () {
+it('Parser#EB->GB', function () {
     expect('234000000000.00')->toBe(ConvertDataSize::parser('234 EB', 'GB'));
 });
 
-it('ConvertTo#GB->Zbit', function () {
+it('Parser#GB->Zbit', function () {
     expect('0.000036542864')->toBe(ConvertDataSize::parser('4567858 GB', 'Zbit', 12));
 });
---
+++ tests/Unit/ConvertUnitToSizeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use Lucasjs7\ConvertDataSize\ConvertDataSize;
+
+it('Unit#KiB->MB', function () {
+    expect('78384470540.63')->toBe(ConvertDataSize::unit('76547334512342', 'KiB', 'MB'));
+});
+
+it('Unit#EB->GB', function () {
+    expect('234000000000.00')->toBe(ConvertDataSize::unit('234', 'EB', 'GB'));
+});
+
+it('Unit#GB->Zbit', function () {
+    expect('0.000036542864')->toBe(ConvertDataSize::unit('4567858', 'GB', 'Zbit', 12));
+});
